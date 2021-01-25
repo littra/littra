995b54cdbc77f644c170990e54e45ed9c74d5f99
aaku14n <akarshgdps@gmail.com>
2021-01-25 17:23:28 +0530

    Reverting change

--- src/Home/Component/Home.js
+++ src/Home/Component/Home.js
@@ -34,43 +34,43 @@ class Home extends React.Component {
   render() {
     return (
       <div className={styles.base}>
-        {/* <div
+        <div
           className={styles.front}
           style={{
-            transform: `translateY(${this.state.translateY}px)`
+            transform: `translateY(${this.state.translateY}px)`,
           }}
         >
           <TopBanner />
-        </div> */}
+        </div>
         <div className={styles.second}>
           {/* <VisibilityChild> */}
-          {/* <About /> */}
+          <About />
           {/* </VisibilityChild> */}
         </div>
         <div className={styles.third}>
           {/* <VisibilityChild> */}
-          {/* <FeatureBanner /> */}
+          <FeatureBanner />
           {/* </VisibilityChild> */}
         </div>
         <div className={styles.second}>
           {/* <VisibilityChild> */}
-          {/* <FeatureTwoBanner /> */}
+          <FeatureTwoBanner />
           {/* </VisibilityChild> */}
         </div>
 
         {/* <VisibilityChild> */}
-        {/* <Portfolio /> */}
+        <Portfolio />
         {/* </VisibilityChild> */}
 
         {/* <VisibilityChild> */}
-        {/* <WhyUs /> */}
+        <WhyUs />
         {/* </VisibilityChild> */}
         {/* 
         <VisibilityChild>
           <Team />
         </VisibilityChild> */}
-        {/* <ContactUs /> */}
-        {/* <Footer /> */}
+        <ContactUs />
+        <Footer />
       </div>
     );
   }
