10864f393b135eb5e8b646f9c7f73e3275915933
Sammed <sammujain03@gmail.com>
2022-07-08 12:06:57 +0530

    AnimatedCardComponent Added

--- src/Home/Component/FeatureTwoBanner.js
+++ src/Home/Component/FeatureTwoBanner.js
@@ -2,43 +2,7 @@ import React from "react";
 import styles from "./css/FeatureTwoBanner.css";
 import { FormattedMessage } from "react-intl";
 import HomeMessages from "../Messages/HomeMessages";
-import Image from "../../general/Image";
-import { PUBLIC_ASSETS_PATH } from "../../Utils/Constants";
-import Icon from "../../general/Icon";
-const leftSection = [
-  {
-    icon: "f1.svg",
-    title: <FormattedMessage {...HomeMessages.f1} />,
-    desc: <FormattedMessage {...HomeMessages.fd1} />,
-  },
-  {
-    icon: "f2.svg",
-    title: <FormattedMessage {...HomeMessages.f2} />,
-    desc: <FormattedMessage {...HomeMessages.fd2} />,
-  },
-  {
-    icon: "f3.svg",
-    title: <FormattedMessage {...HomeMessages.f3} />,
-    desc: <FormattedMessage {...HomeMessages.fd3} />,
-  },
-];
-const rightSection = [
-  {
-    icon: "f4.svg",
-    title: <FormattedMessage {...HomeMessages.f4} />,
-    desc: <FormattedMessage {...HomeMessages.fd4} />,
-  },
-  {
-    icon: "f5.svg",
-    title: <FormattedMessage {...HomeMessages.f5} />,
-    desc: <FormattedMessage {...HomeMessages.fd5} />,
-  },
-  {
-    icon: "f6.svg",
-    title: <FormattedMessage {...HomeMessages.f6} />,
-    desc: <FormattedMessage {...HomeMessages.fd6} />,
-  },
-];
+import AnimatedCard from "../../WebSite Ui Components/FlexCard/AnimatedCard";
 export default class FeatureTwoBanner extends React.Component {
   constructor(props) {
     super(props);
@@ -63,40 +27,7 @@ export default class FeatureTwoBanner extends React.Component {
   componentWillUnmount() {
     window.removeEventListener("scroll");
   }
-  renderLeftSection = () => {
-    return leftSection.map((item, id) => {
-      return (
-        <div className={styles.smallCard}>
-          <div className={styles.leftCard}>
-            <h2 className={styles.dark}>{item.title}</h2>
-            <p className={styles.desc}>{item.desc}</p>
-          </div>
-          <div className={styles.rightCard}>
-            <div className={styles.iconWrapper}>
-              <Icon image={`${PUBLIC_ASSETS_PATH}/${item.icon}`} size={25} />
-            </div>
-          </div>
-        </div>
-      );
-    });
-  };
-  renderRightSection = () => {
-    return rightSection.map((item, id) => {
-      return (
-        <div className={styles.smallCard}>
-          <div className={styles.rightCard}>
-            <div className={styles.iconWrapper}>
-              <Icon image={`${PUBLIC_ASSETS_PATH}/${item.icon}`} size={25} />
-            </div>
-          </div>
-          <div className={styles.leftCardWithRight}>
-            <h2 className={styles.dark}>{item.title}</h2>
-            <p className={styles.desc}>{item.desc}</p>
-          </div>
-        </div>
-      );
-    });
-  };
+
   render() {
     return (
       <div className={styles.base} id="pages">
@@ -110,31 +41,7 @@ export default class FeatureTwoBanner extends React.Component {
         </div>
         <hr className={styles.divider} />
         <div className={styles.infoWrapper}>
-          <div className={styles.imageFrame}>
-            <div
-              className={styles.backgroundImage}
-              style={{
-                transform: `translateY(${this.state.translateY}px)`,
-              }}
-            >
-              <Image
-                src={`${PUBLIC_ASSETS_PATH}/mobileWallpaper.jpg`}
-                paddingBottom={"100vh"}
-              />
-            </div>
-          </div>
-          <div className={styles.info}>
-            <div className={styles.leftInfo}>{this.renderLeftSection()}</div>
-            <div className={styles.imageWrapper}>
-              <div className={styles.image}>
-                <Image
-                  src={`${PUBLIC_ASSETS_PATH}/mobile.png`}
-                  paddingBottom="450px"
-                />
-              </div>
-            </div>
-            <div className={styles.rightInfo}>{this.renderRightSection()}</div>
-          </div>
+          <AnimatedCard />
         </div>
       </div>
     );
---
+++ src/WebSite Ui Components/FlexCard/AnimatedCard.js
@@ -0,0 +1,106 @@
+import React from "react";
+import styles from "./AnimatedCard.css";
+import Icon from "../../general/Icon";
+import { PUBLIC_ASSETS_PATH } from "../../Utils/Constants";
+import { FormattedMessage } from "react-intl";
+import HomeMessages from "../../Home/Messages/HomeMessages";
+
+function AnimatedCard() {
+  const AnimatedCardData = [
+    {
+      icon: "f1.svg",
+      title: <FormattedMessage {...HomeMessages.f1} />,
+      desc: <FormattedMessage {...HomeMessages.fd1} />,
+      image: "LayoutDesign.jpg",
+    },
+    {
+      icon: "f2.svg",
+      title: <FormattedMessage {...HomeMessages.f2} />,
+      desc: <FormattedMessage {...HomeMessages.fd2} />,
+      image: "documentation.jpg",
+    },
+    {
+      icon: "f3.svg",
+      title: <FormattedMessage {...HomeMessages.f3} />,
+      desc: <FormattedMessage {...HomeMessages.fd3} />,
+      image: "parallex.png",
+    },
+
+    {
+      icon: "f4.svg",
+      title: <FormattedMessage {...HomeMessages.f4} />,
+      desc: <FormattedMessage {...HomeMessages.fd4} />,
+      image: "html5.jpg",
+    },
+    {
+      icon: "f5.svg",
+      title: <FormattedMessage {...HomeMessages.f5} />,
+      desc: <FormattedMessage {...HomeMessages.fd5} />,
+      image: "support.jpg",
+    },
+    {
+      icon: "f6.svg",
+      title: <FormattedMessage {...HomeMessages.f6} />,
+      desc: <FormattedMessage {...HomeMessages.fd6} />,
+      image: "LightDark.jpg",
+    },
+  ];
+
+  function MouseEnter(event) {
+    event.target.classList.toggle(styles.active);
+  }
+
+  function MouseLeave(event) {
+    event.target.classList.remove(styles.active);
+  }
+
+  return (
+    <div className={styles.AnimatedCardMain}>
+      <div className={styles.AnimatedCardOptions}>
+        {AnimatedCardData?.map((item, index) => {
+          console.log("===", item.image);
+          return (
+            <div
+              className={styles.AnimatedCardOption}
+              onMouseEnter={MouseEnter}
+              onMouseLeave={MouseLeave}
+              style={{
+                backgroundImage: `url(${PUBLIC_ASSETS_PATH}/${item.image})`,
+                backgroundRepeat: "no-repeat",
+                marginRight: "10px",
+              }}
+            >
+              <div className={styles.shadow}></div>
+              <div className={styles.label}>
+                <div className={styles.icon}>
+                  <Icon
+                    image={`${PUBLIC_ASSETS_PATH}/${item.icon}`}
+                    size={20}
+                  />
+                </div>
+                <div className={styles.info}>
+                  <div className={styles.title}>
+                    <p> {item.title} </p>
+                  </div>
+                  <div className={styles.sub} style={{ padding: "5px" }}>
+                    <p> {item.desc} </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <link
+        rel="stylesheet"
+        href="https://static.fontawesome.com/css/fontawesome-app.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://pro.fontawesome.com/releases/v5.2.0/css/all.css"
+      />
+    </div>
+  );
+}
+
+export default AnimatedCard;
