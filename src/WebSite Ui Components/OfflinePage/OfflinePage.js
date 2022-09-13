a93907b65d0567cd4a6cc711b009eb387631c6dc
Sammed <sammujain03@gmail.com>
2022-09-13 11:28:53 +0530

    Offline Components Added

---
+++ src/WebSite Ui Components/OfflinePage/OfflinePage.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import "!style-loader!css-loader!./OfflinePage.css";
+import { PUBLIC_ASSETS_PATH } from "../../Utils/Constants";
+import PlanetDefense from "./PlanetDefense";
+import Draw from "./Draw";
+
+function OfflinePage() {
+  const [offline, setOffline] = useState(false);
+  const [modeOne, setModeOne] = useState(false);
+  const [modeTwo, setModeTwo] = useState(false);
+
+  useEffect(() => {
+    if (!navigator.onLine) setOffline(true);
+  }, []);
+
+  const PlanetGame = () => {
+    setModeOne(true);
+  };
+  const BlasterGame = () => {
+    setModeTwo(true);
+  };
+  return (
+    <>
+      {offline && (
+        <div>
+          {modeTwo ? <Draw /> : null}
+          {modeOne ? <PlanetDefense /> : null}
+          {!modeTwo && !modeOne ? (
+            <div className="offlineWrapper">
+              <div className="brandTitle">LITTRA</div>
+              <hr className="border" />
+              <div className="refreshDiv">
+                <div className="offlineText">
+                  You are offline please connect to the internet
+                </div>
+                <button
+                  onClick={() => window.location.reload(true)}
+                  className="gameBtn"
+                >
+                  Refresh
+                </button>
+              </div>
+              <div className="cardWrapper">
+                <div className="cardContainer">
+                  <div className="column">
+                    <div className="card">
+                      <div className="content">
+                        <div className="front">
+                          <img
+                            className="profile"
+                            width="100%"
+                            src={`${PUBLIC_ASSETS_PATH}/planetDefense.png`}
+                            alt="PlanetImg"
+                          />
+                        </div>
+                        <div className="back from-bottom">
+                          <div className="textDark">
+                            <span className="lineBreak"> Play </span>
+                            <div className="gameText" onClick={PlanetGame}>
+                              Planet-Game
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="cardContainer">
+                  <div className="column">
+                    <div className="card">
+                      <div className="content">
+                        <div className="front">
+                          <img
+                            className="profile"
+                            width="100%"
+                            src={`${PUBLIC_ASSETS_PATH}/CanvasDraw.png`}
+                            alt="BlasterImg"
+                          />
+                        </div>
+                        <div className="back from-bottom">
+                          <div className="textDark">
+                            <span className="lineBreak"> Draw </span>
+                            <div className="gameText" onClick={BlasterGame}>
+                              Color-Orbitals
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : null}
+          {!offline && null}
+        </div>
+      )}
+    </>
+  );
+}
+
+export default OfflinePage;
